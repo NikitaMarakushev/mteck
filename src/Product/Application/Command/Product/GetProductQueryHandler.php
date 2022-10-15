0a480ca1ec3bad0679f90017f142901f9d4d54e7
Nikita Marakushev <63608270+NikitaMarakushev@users.noreply.github.com>
2022-10-15 20:31:51 +0300

    Fixed bug with db driver, added makefile, GetProductQueryHandler.php and some changes in enviroment

---
+++ src/Product/Application/Command/Product/GetProductQueryHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Product\Application\Command\Product;
+
+use App\Core\Application\Query\QueryHandlerInterface;
+use App\Product\Application\DTO\ProductDTO;
+use App\Product\Domain\Repository\ProductRepositoryInterface;
+
+class GetProductQueryHandler implements QueryHandlerInterface
+{
+    public function __construct(private readonly ProductRepositoryInterface $productRepository)
+    {
+    }
+
+    public function __invoke(GetProductQuery $query): ProductDTO
+    {
+        $product = $this->productRepository->find($query->id);
+
+        return ProductDTO::fromEntity($product);
+    }
+}
