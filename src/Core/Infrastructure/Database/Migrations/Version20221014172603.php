c7ac78abc7b9ab94ccc364f3c358be342137409b
Nikita Marakushev <63608270+NikitaMarakushev@users.noreply.github.com>
2022-10-15 21:11:41 +0300

    migration

---
+++ src/Core/Infrastructure/Database/Migrations/Version20221014172603.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Infrastructure\Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221014172603 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("CREATE TABLE product (
+            id serial PRIMARY KEY,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            price FLOAT NOT NULL,
+            image TEXT NOT NULL
+        )");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("DROP TABLE product");
+    }
+}
